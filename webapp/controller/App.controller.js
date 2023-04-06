44901326f4a1963558eaf3eaf7324f896b63c655
Shubham Sharma <shubham78067@gmail.com>
2023-04-06 10:31:20 +0530

    project 10 start

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("FioriTilesfor10Projects.projects10.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["FioriTilesfor10Projects/projects10/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});

5f2a8801eaa65fb5167b6b24b0d32bef8441611e
Shubham Sharma <shubham78067@gmail.com>
2023-04-07 18:08:22 +0530

    covid app done

--- webapp/controller/covid/Graph.controller.js
+++ webapp/controller/covid/Graph.controller.js
@@ -1,21 +1,42 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    'sap/viz/ui5/api/env/Format',
+    'sap/viz/ui5/format/ChartFormatter'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller,JSONModel,Format,ChartFormatter) {
         "use strict";
 
         return Controller.extend("FioriTilesfor10Projects.projects10.controller.covid.Graph", {
+            dataPath : "https://api.rootnet.in/covid19-in/stats/latest",
+            oVizFrame : null,
             onInit: function () {
-                let myTilesModel = new JSONModel("../model/tiles.json");
-                this.getView().setModel(myTilesModel,"tiles");
+                Format.numericFormatter(ChartFormatter.getInstance());
+                var formatPattern = ChartFormatter.DefaultPattern;
+         
+             
+    
+                var oVizFrame = this.oVizFrame = this.getView().byId("idVizFramePie");
+                oVizFrame.setVizProperties({
+                    legend: {
+                        title: {
+                            visible: false
+                        }
+                    },
+                    title: {
+                        visible: false
+                    }
+                });
+                var myLatestCovidModel = new JSONModel(this.dataPath);
+                oVizFrame.setModel(myLatestCovidModel);
 
-            },
-            press:function (oRoute){
-                this.getOwnerComponent().getRouter().navTo(oRoute);
+                var oPopOver = this.getView().byId("idPopOverPie");
+                oPopOver.connect(oVizFrame.getVizUid());
+                oPopOver.setFormatString(formatPattern.STANDARDFLOAT);
+
             }
         });
     });

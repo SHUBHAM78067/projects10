38f57af2a6710ed206e2ae40cd11028afeb3b29b
Shubham Sharma <shubham78067@gmail.com>
2023-04-07 13:19:34 +0530

    viz line chart completed of project2

---
+++ webapp/controller/covid/Covid.controller.js
@@ -0,0 +1,70 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    'sap/viz/ui5/api/env/Format',
+    'sap/viz/ui5/format/ChartFormatter'
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,JSONModel,Format,ChartFormatter) {
+        "use strict";
+
+        return Controller.extend("FioriTilesfor10Projects.projects10.controller.covid.Covid", {
+            dataPath : "../model/covid.json",
+            oVizFrame : null,
+           
+            onInit: function () {
+                debugger;
+                Format.numericFormatter(ChartFormatter.getInstance());
+                var formatPattern = ChartFormatter.DefaultPattern;
+         
+             
+    
+                var oVizFrame = this.oVizFrame = this.getView().byId("idVizFrame");
+                oVizFrame.setVizProperties({
+                    plotArea: {
+                        dataLabel: {
+                            formatString:formatPattern.SHORTFLOAT_MFD2,
+                            visible: true
+                        }
+                    },
+                    valueAxis: {
+                        label: {
+                            formatString: formatPattern.SHORTFLOAT
+                        },
+                        title: {
+                            visible: false
+                        }
+                    },
+                    categoryAxis: {
+                        title: {
+                            visible: false
+                        }
+                    },
+                    title: {
+                        visible: false,
+                        text: 'Covid Cases History'
+                    }
+                });
+                var dataModel = new JSONModel(this.dataPath);
+                oVizFrame.setModel(dataModel);
+    
+                var oPopOver = this.getView().byId("idPopOver");
+                oPopOver.connect(oVizFrame.getVizUid());
+                oPopOver.setFormatString(formatPattern.STANDARDFLOAT);
+    
+                // InitPageUtil.initPageSettings(this.getView());
+
+                // let myCovidModel = new JSONModel("../model/covid.json");
+                // this.getView().setModel(myCovidModel,"covid");
+            },
+            onPressList:function () {
+                alert("list");
+            },
+            onPressGraph:function () {
+                alert("graph");
+            }
+          
+        });
+    });

abb83f5278d4f6ae95854e1fc8b5f0a687195ed2
Shubham Sharma <shubham78067@gmail.com>
2023-04-08 23:47:38 +0530

    Project 3 Routing done

---
+++ webapp/controller/vacc/Vacc.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,JSONModel) {
+        "use strict";
+
+        return Controller.extend("FioriTilesfor10Projects.projects10.controller.vacc.Vacc", {
+            onInit: function () {
+            
+
+            },
+            press:function (oRoute){
+       
+            }
+        });
+    });

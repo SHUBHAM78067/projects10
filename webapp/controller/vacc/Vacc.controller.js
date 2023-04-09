8001119fa76c12b4a56ceaeb62b17045c3a0e484
Shubham Sharma <shubham78067@gmail.com>
2023-04-09 22:51:22 +0530

    Project 3 Completed

--- webapp/controller/vacc/Vacc.controller.js
+++ webapp/controller/vacc/Vacc.controller.js
@@ -10,11 +10,32 @@ sap.ui.define([
 
         return Controller.extend("FioriTilesfor10Projects.projects10.controller.vacc.Vacc", {
             onInit: function () {
-            
+                let myVaccModel = new JSONModel("../model/vacc.json");
+                this.getView().setModel(myVaccModel,"vacc");
+
+                let myViewConfig = {
+                    "table":true,
+                    "calendar":false
+                };
+                let myViewModel = new JSONModel(myViewConfig);
+                this.getView().setModel(myViewModel,"view");
 
             },
-            press:function (oRoute){
-       
+            formatDate:function (input) {
+                return new Date(input);        
+            },
+            formatState:function (input) {
+                let currentDate = new Date();
+                let inputDate = new Date(input);
+                if(inputDate < currentDate){
+                    return 'Success'; 
+                }else if(inputDate > currentDate){
+                    return 'Warning';
+                }else{
+                    return 'Error';
+                }
+                
             }
         });
+
     });

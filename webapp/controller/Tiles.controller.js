ce0bd41394d992e3f263d195b32450b60af19815
Shubham Sharma <shubham78067@gmail.com>
2023-04-10 10:48:20 +0530

    ok

--- webapp/controller/Tiles.controller.js
+++ webapp/controller/Tiles.controller.js
@@ -10,6 +10,7 @@ sap.ui.define([
 
         return Controller.extend("FioriTilesfor10Projects.projects10.controller.Tiles", {
             onInit: function () {
+                
                 let myTilesModel = new JSONModel("../model/tiles.json");
                 this.getView().setModel(myTilesModel,"tiles");
 
@@ -17,6 +18,7 @@ sap.ui.define([
             press:function (oRoute){
                 if(oRoute.substring(0,4) == 'EXT-'){
                   let selItem =  JSON.parse(this.getView().getModel("tiles").getJSON()).find(item => {if(item.route == oRoute){return item;}});
+                  // URLHelper is used to route out side the app(call,sms,email,url)
                   sap.m.URLHelper.redirect(selItem.url);
                 }else{
                     this.getOwnerComponent().getRouter().navTo(oRoute);

e9ff59be4679b245babc66fd4904bb0b8bb16fd4
Shubham Sharma <shubham78067@gmail.com>
2023-05-11 13:24:11 +0530

    request raised popup msg done

--- webapp/controller/bcr/Bcr.controller.js
+++ webapp/controller/bcr/Bcr.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller,JSONModel, MessageBox, MessageToast) {
         "use strict";
 
         return Controller.extend("FioriTilesfor10Projects.projects10.controller.bcr.Bcr", {
@@ -24,6 +26,23 @@ sap.ui.define([
            onpressar:function (event) {
             this.getView().byId("SimpleFormToolbarnr").setVisible(false);
             this.getView().byId("idsave").setVisible(false);
+           },
+           onPressSave:function (event) {
+            var oResourceModel = this.getView().getModel("i18n");
+            var oBundle = oResourceModel.getResourceBundle();
+            var msgSuccess = oBundle.getText("msgSuccess", ["858585"]);
+            var msgError = oBundle.getText("msgError");
+    
+            MessageBox.confirm("Do you want to raise a new request ?", {
+              title: "Confirmation",
+              onClose: function (status) {
+                if (status === "OK") {
+                  MessageToast.show(msgSuccess);
+                } else {
+                  MessageBox.error(msgError);
+                }
+              },
+            });
            }
         });
     });
